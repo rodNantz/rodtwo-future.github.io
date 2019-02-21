fb53e358fdf8f5bf979123340faba5064dd0c677
Rodrigo Nantes dos Santos <r.santos@cinq.com.br>
2019-02-21 18:20:58 -0300

    brvnks

--- js/ipsum.js
+++ js/ipsum.js
@@ -1,5 +1,5 @@
 
-export var wordsStr = 
+var wordsStr = 
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. "+
     "Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. "+
     "Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. "+
@@ -10,4 +10,3 @@ export var wordsStr =
     "Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. "+
     "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. "+
     "Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. ";
-
